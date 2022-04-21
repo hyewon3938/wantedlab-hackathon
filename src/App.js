4389b71a43be22264df8406ed0f2616b20594806
hyewon3938 <hyewon3938@naver.com>
2022-04-21 22:40:26 +0900

    fix : 라우터 hash라우터로 변경

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import GlobalStyleProvider from "./components/style/GlobalStyleProvider";
 import Main from "./components/Main/Main";
