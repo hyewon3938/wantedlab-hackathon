ea68463c49d907cda3a5a6fec5bfd133601dcc3d
hyewon3938 <hyewon3938@naver.com>
2022-04-22 00:01:50 +0900

    fix : 깃헙페이지 배포용 404html 추가

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Result from "./components/Survey/Result";
 const App = () => {
   return (
     <GlobalStyleProvider>
-      <Router basename={"test-survey-generator"}>
+      <Router basename={"/test-survey-generator"}>
         <Switch>
           <Route exact path="/" component={Main} />
           <Route path="/survey" component={SurveyMain} />
